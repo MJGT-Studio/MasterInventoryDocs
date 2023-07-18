e37495fb067b3c7ccf8a3adba692642cbcf7c849
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2023-07-18 20:16:07 +0100

    revert
    This reverts commit 5128cf093606cb46c487aea0d047ef3fdc98cc44.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,45 +10,32 @@ import rehypeKatex from 'rehype-katex';
 import remarkPlantUML from '@akebifiky/remark-simple-plantuml';
 import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
 import { remarkDiagram } from './remark-plugins/remark-diagram.mjs';
-import partytown from "@astrojs/partytown";
+
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        ssr: {
-            external: ['svgo'],
-            noExternal: ['swiper', 'leaflet']
-        }
-    },
-    site: "https://mjgt-studios.github.io",
-    base: '/MasterInventoryDocs',
-    integrations: [
-        tailwind(),
-        sitemap(),
-        mdx(),
-        alpinejs(),
-        robotsTxt(),
-        markdoc(),
-        partytown({
-            // Adds dataLayer.push as a forwarding-event.
-            config: {
-                forward: ["dataLayer.push"],
-            },
-        }),
-    ],
-    experimental: {
-        assets: true
-    },
-    markdown: {
-        extendDefaultPlugins: true,
-        remarkPlugins: [remarkReadingTime, remarkMath, remarkPlantUML, remarkDiagram, remarkEmoji],
-        rehypePlugins: [rehypeKatex],
-        shikiConfig: {
-            theme: 'github-light',
-            langs: [],
-            // Enable word wrap to prevent horizontal scrolling
-            wrap: true
-        }
+  vite: {
+    ssr: {
+      external: ['svgo'],
+      noExternal: ['swiper', 'leaflet']
     }
+  },
+  site: "https://mjgt-studios.github.io",
+  base: '/MasterInventoryDocs',
+  integrations: [tailwind(), sitemap(), mdx(), alpinejs(), robotsTxt(), markdoc()],
+  experimental: {
+    assets: true
+  },
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [remarkReadingTime, remarkMath, remarkPlantUML, remarkDiagram, remarkEmoji],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      theme: 'github-light',
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  }
 });
