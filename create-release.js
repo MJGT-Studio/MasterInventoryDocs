73a6d1ef039a0eda712e43347d068842869036da
Mario Tarosso <mtarosso@frontiner.co.uk>
2024-01-30 09:36:14 +0000

    autodopcs deploy

--- create-release.js
+++ create-release.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
 
 const releaseVersion = process.env.GITHUB_REF.split('/').pop();
 
@@ -10,7 +10,7 @@ const releaseInfo = `
 Add your release notes here.
 `;
 
-const filePath = path.join(__dirname, 'src/content/blog', `release-${releaseVersion}.md`);
+const filePath = path.join(__dirname, 'path/to/your/folder', `release-${releaseVersion}.md`);
 
 fs.writeFileSync(filePath, releaseInfo.trim());
 
