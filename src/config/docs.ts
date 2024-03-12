6c9f937fd91ecef34ab730e515f7d00f6c45a15d
Mario Tarosso <mariojgt2@gmail.com>
2024-03-12 08:19:38 +0000

    node-projects/master-inventory-docs/project/MasterInventoryDocs automatic push

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -1,115 +1,139 @@
-import type { DocsConfig } from "@/types"
+import type { DocsConfig } from "@/types";
 
 export const docsConfig: DocsConfig = {
-//   mainNav: [
-//     {
-//       title: "Documentation",
-//       href: "/docs/getting-started/",
-//     },
-//     {
-//       title: "Guides",
-//       href: "/guides",
-//     },
-//   ],
-  sidebarNav: [
-    // {
-    //   title: "Getting Started",
-    //   items: [
+    //   mainNav: [
     //     {
-    //       title: "Introduction",
+    //       title: "Documentation",
     //       href: "/docs/getting-started/",
     //     },
-    //   ],
-    // },
-    {
-      title: "Master Inventory",
-      items: [
-        {
-          title: "Introduction 🫡 (docs)",
-          href: "/MasterInventoryDocs/docs/documentation/master-inventory-index",
-        },
-        {
-            title: "Get Started 🚀 (video)",
-            href: "https://www.youtube.com/watch?v=uoZVvRGFyJc",
-        },
-        {
-          title: "Talent System 🧙‍♂️ (video)",
-          href: "https://www.youtube.com/watch?v=GsixQA2m5Yw",
-        },
-        {
-          title: "Theme Swamp 🌲 (video)",
-          href: "https://www.youtube.com/watch?v=F4Vg33ro8pU",
-        },
-        {
-          title: "Item Attributes 🧬 (video)",
-          href: "https://www.youtube.com/watch?v=eNHuHR0dG0c",
-        },
-        {
-            title: "Add Item ⚔️ (video)",
-            href: "https://www.youtube.com/watch?v=Hs5s3aQMpUs&t=3s",
-        },
-        {
-            title: "Equip Item 🤓 (video)",
-            href: "https://www.youtube.com/watch?v=HXMW3pyxwuM",
-        },
-        {
-            title: "Item Create ⚡ (video)",
-            href: "https://www.youtube.com/watch?v=CByNzRU9L0E",
-        }
-      ],
-    },
-    {
-      title: "Master UI",
-      items: [
-        {
-          title: "Introduction 🫡 (docs)",
-          href: "/MasterInventoryDocs/docs/documentation/master-ui-index",
-        }
-      ],
-    },
-    {
-      title: "Master Request",
-      items: [
-        {
-          title: "Get started 🚀 (docs)",
-          href: "/MasterInventoryDocs/docs/documentation/master-request-index",
-          disabled: false,
-        }
-      ],
-    },
-    {
-        title: "Master Chat",
-        items: [
-          {
-            title: "Introduction 🫡 (docs)",
-            href: "/MasterInventoryDocs/docs/documentation/master-ui-index",
-          }
-        ],
-      },
-    // {
-    //   title: "Marketing Site",
-    //   items: [
     //     {
-    //       title: "Introduction",
-    //       href: "/docs/in-progress",
-    //       disabled: true,
-    //     },
-    //     {
-    //       title: "File Structure",
-    //       href: "/docs/in-progress",
-    //       disabled: true,
-    //     },
-    //     {
-    //       title: "Tailwind CSS",
-    //       href: "/docs/in-progress",
-    //       disabled: true,
-    //     },
-    //     {
-    //       title: "Typography",
-    //       href: "/docs/in-progress",
-    //       disabled: true,
+    //       title: "Guides",
+    //       href: "/guides",
     //     },
     //   ],
-    // },
-  ],
-}
+    sidebarNav: [
+        // {
+        //   title: "Getting Started",
+        //   items: [
+        //     {
+        //       title: "Introduction",
+        //       href: "/docs/getting-started/",
+        //     },
+        //   ],
+        // },
+        {
+            title: "Master Inventory",
+            items: [
+                {
+                    title: "Introduction 🫡 (docs)",
+                    href: "/MasterInventoryDocs/docs/documentation/master-inventory-index",
+                },
+                {
+                    title: "Get Started 🚀 (video)",
+                    href: "https://www.youtube.com/watch?v=uoZVvRGFyJc",
+                },
+                {
+                    title: "Talent System 🧙‍♂️ (video)",
+                    href: "https://www.youtube.com/watch?v=GsixQA2m5Yw",
+                },
+                {
+                    title: "Theme Swamp 🌲 (video)",
+                    href: "https://www.youtube.com/watch?v=F4Vg33ro8pU",
+                },
+                {
+                    title: "Item Attributes 🧬 (video)",
+                    href: "https://www.youtube.com/watch?v=eNHuHR0dG0c",
+                },
+                {
+                    title: "Add Item ⚔️ (video)",
+                    href: "https://www.youtube.com/watch?v=Hs5s3aQMpUs&t=3s",
+                },
+                {
+                    title: "Equip Item 🤓 (video)",
+                    href: "https://www.youtube.com/watch?v=HXMW3pyxwuM",
+                },
+                {
+                    title: "Item Create ⚡ (video)",
+                    href: "https://www.youtube.com/watch?v=CByNzRU9L0E",
+                },
+            ],
+        },
+        {
+            title: "Master UI",
+            items: [
+                {
+                    title: "Introduction 🫡 (docs)",
+                    href: "/MasterInventoryDocs/docs/documentation/master-ui-index",
+                },
+            ],
+        },
+        {
+            title: "Master Request",
+            items: [
+                {
+                    title: "Get started 🚀 (docs)",
+                    href: "/MasterInventoryDocs/docs/documentation/master-request-index",
+                    disabled: false,
+                },
+                {
+                    title: "Get Started ✔️ (video)",
+                    href: "https://www.youtube.com/watch?v=N54sMzA37CI",
+                },
+            ],
+        },
+        {
+            title: "Master Chat",
+            items: [
+                {
+                    title: "Introduction 🫡 (docs)",
+                    href: "/MasterInventoryDocs/docs/documentation/master-chat",
+                },
+                {
+                    title: "Get Started ✔️ (video)",
+                    href: "https://www.youtube.com/watch?v=1eLBKRfYYuM",
+                },
+                {
+                    title: "Emoji and Avatar 🤓 (video)",
+                    href: "https://www.youtube.com/watch?v=9bMtIOHHfJ8",
+                },
+                {
+                    title: "Profanity Filter 🙄 (video)",
+                    href: "https://www.youtube.com/watch?v=XVqR2l_MxGQ",
+                },
+                {
+                    title: "Commands 📃 (video)",
+                    href: "https://www.youtube.com/watch?v=81nCFN9NFPg",
+                },
+                {
+                    title: "Area Chat 🥸 (video)",
+                    href: "https://www.youtube.com/watch?v=wPB3ABDJfpc",
+                },
+            ],
+        },
+        // {
+        //   title: "Marketing Site",
+        //   items: [
+        //     {
+        //       title: "Introduction",
+        //       href: "/docs/in-progress",
+        //       disabled: true,
+        //     },
+        //     {
+        //       title: "File Structure",
+        //       href: "/docs/in-progress",
+        //       disabled: true,
+        //     },
+        //     {
+        //       title: "Tailwind CSS",
+        //       href: "/docs/in-progress",
+        //       disabled: true,
+        //     },
+        //     {
+        //       title: "Typography",
+        //       href: "/docs/in-progress",
+        //       disabled: true,
+        //     },
+        //   ],
+        // },
+    ],
+};
